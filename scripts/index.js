49af1245e266255629eea78c4312f5107f7cdace
Kirill Zemko <kirillzemko@yandex.ru>
2021-06-11 10:19:34 +0200

    feat: добавить закрытие popup по клику на overflow

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,24 @@ let closeBtn = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
-let profileName = document.querySelector('.profile__name')
-let profileJob = document.querySelector('.profile__job')
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let popupOverflow = document.querySelector('.popup');
 
+// функция открытия popup окна и добавления значенний в input-ы из profileName
 function editPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
 }
 
+// функция закрытия окна popup
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+// функция изменяет значения введенных пользователем в input-ы и изменяет profileName, profileJob
+function formSubmitHandler(evt) {
     // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки.
     evt.preventDefault();
 
@@ -27,6 +31,16 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 
+// функция закрытия popup по клику по затемненной области
+function closePopupByClickOnOverflow(evt) {
+   if (evt.target !== evt.currentTarget) {
+     return;
+   }
+
+  closePopup();
+}
+
 editBtn.addEventListener('click', editPopup);
 closeBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+popupOverflow.addEventListener('click', closePopupByClickOnOverflow);
